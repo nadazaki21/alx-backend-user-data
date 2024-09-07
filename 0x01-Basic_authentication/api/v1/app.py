309323a9a5fd71043dfbc1d656fe2c97fd3ff553
nzaki1 <nzaki@lucidya.com>
2024-09-07 13:10:27 +0300

    2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,11 +27,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
